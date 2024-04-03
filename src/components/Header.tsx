5b327bc7543f5a095292b4bf073379208c12fd41
Jim-Dillon <james.dillon94@gmail.com>
2024-04-03 11:27:47 +0100

    add nav dropdown

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,55 @@
-import { Link } from "react-router-dom";
-import styles from '../css/header.module.scss'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../css/header.module.scss';
+import logo from '/src/assets/img/SD_Logo.svg';
 
 const Header = () => {
-    return (
-        <>
-        <div className={styles.navItems}>
-            <Link to="/about">About</Link>
-            <Link to="/exhibitions">exhibitions</Link>
-            <Link to="/showcase">showcase</Link>
-            <Link to="/contact">contact</Link>
-        </div>    
-        </> 
-      )
-}
+    const [showDropdown, setShowDropdown] = useState(false);
 
-export default Header
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    };
+
+    return (
+        <header>
+            <Link to="/">
+                <img src={logo} alt="" />
+            </Link>
+            <div className={styles.navRight}>
+                <div className={styles.topNav}>
+                    <Link to="#" className={styles.dropdownLink} onClick={toggleDropdown}>
+                        <svg
+                            className={styles.dropdownIcon}
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="30"
+                            height="30"
+                            viewBox="0 0 30 30"
+                        >
+                            <path d="M4 6h22M4 15h22M4 24h22" strokeWidth="2" strokeLinecap="round" />
+                        </svg>
+                    </Link>
+                </div>
+            </div>
+            <div className={`${styles.dropdownMenuContainer} ${showDropdown ? styles.showDropdown : ''}`}>
+                <div className={styles.dropdownMenu}>
+                    <ul>
+                        <li className={styles.dropdownItem}>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li className={styles.dropdownItem}>
+                            <Link to="/exhibitions">Exhibitions</Link>
+                        </li>
+                        <li className={styles.dropdownItem}>
+                            <Link to="/showcase">Showcase</Link>
+                        </li>
+                        <li className={styles.dropdownItem}>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;

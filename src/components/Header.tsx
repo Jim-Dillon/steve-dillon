5d986935fcd68123ce075e84ea317a3d4b3f5cd9
Jim-Dillon <james.dillon94@gmail.com>
2024-04-03 09:20:37 +0100

    add test styles to nav headers

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
+import styles from '../css/header.module.scss'
 
 const Header = () => {
     return (
         <>
+        <div className={styles.navItems}>
             <Link to="/about">About</Link>
             <Link to="/exhibitions">exhibitions</Link>
             <Link to="/showcase">showcase</Link>
             <Link to="/contact">contact</Link>
+        </div>    
         </> 
       )
 }

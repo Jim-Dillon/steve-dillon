e17c7f773f17f948ca7c4ef40e0cbd12b6cd1bf3
Jim-Dillon <james.dillon94@gmail.com>
2024-04-04 16:07:46 +0100

    add contact page card - mobile

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -6,7 +6,13 @@ const ContactPage = () => {
     <>
       <Header/>
       <div className={styles.contactPageSection}>
-        
+        <div className={styles.contactContent}>
+          <h1>Get in touch</h1>
+          <p>If you’re interested in including the Steve Dillon Exhibition in your upcoming show, please feel free to reach out and say hello... </p>
+          <h2>Email us</h2>
+          <p>exhibitions.stevedillon@gmail.com</p>
+          <h2>Follow us</h2>
+        </div>
       </div>
     </>
     

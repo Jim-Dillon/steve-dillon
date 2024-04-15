0e14800ebca7154034cb4692e8e3ad9e7f79f33f
Jim-Dillon <james.dillon94@gmail.com>
2024-04-15 14:55:13 +0100

    add basename prop to router

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import ContactPage from './pages/ContactPage';
 const App = () => {
   
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/about' element={<AboutPage />}>

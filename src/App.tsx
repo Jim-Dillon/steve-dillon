f60b3ce36ab8748ace58d02ba6b81c39732ea5f4
Jim-Dillon <james.dillon94@gmail.com>
2024-04-16 14:09:00 +0100

    remove unnecessary Router code from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,8 @@
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// // import { Link } from "react-router-dom";
-// import HomePage from './pages/HomePage';
-// import AboutPage from './pages/AboutPage';
-// import ExhibitionsPage from './pages/ExhibitionsPage';
-// import ShowcasePage from './pages/ShowcasePage';
-// import ContactPage from './pages/ContactPage';
 import { Outlet } from "react-router-dom"
 
 const App = () => {
   
   return (
-    // <Router>
-    //   <Routes>
-    //     <Route path='/steve-dillon' element={<HomePage />} />
-    //     <Route path='/about' element={<AboutPage />}>
-    //     </Route>
-    //     <Route path='/exhibitions' element={<ExhibitionsPage />} />
-    //     <Route path='/showcase' element={<ShowcasePage />} />
-    //     <Route path='/contact' element={<ContactPage />} />
-    //   </Routes>
-    // </Router>
     <Outlet />
   )
 }

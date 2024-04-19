7dddef3253727c6a01c034f49815c9945c1d4142
Jim-Dillon <james.dillon94@gmail.com>
2024-04-19 11:15:12 +0100

    showcase - add new images

--- src/components/imageData.tsx
+++ src/components/imageData.tsx
@@ -2,6 +2,10 @@ import image1 from "/src/assets/img/2000ad.png"
 import image2 from "/src/assets/img/enniskillen.png"
 import image3 from "/src/assets/img/lawless.png"
 
-const imageData = [image1, image2, image3];
+const imageData = [
+    image1, 
+    image2, 
+    image3
+];
 
 export default imageData;

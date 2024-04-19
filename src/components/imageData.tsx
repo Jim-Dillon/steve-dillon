0ef1fd472fb0d68075666e89a4e1436ef75077f2
Jim-Dillon <james.dillon94@gmail.com>
2024-04-19 11:54:46 +0100

    showcase - swap all images

--- src/components/imageData.tsx
+++ src/components/imageData.tsx
@@ -1,11 +1,79 @@
-import image1 from "/src/assets/img/2000ad.png"
-import image2 from "/src/assets/img/enniskillen.png"
-import image3 from "/src/assets/img/lawless.png"
+import image1 from "/src/assets/img/showcase/1 preacher.png"
+import image2 from "/src/assets/img/showcase/2 hellblazer.png"
+import image3 from "/src/assets/img/showcase/3 hulk.png"
+import image4 from "/src/assets/img/showcase/4 dredd.png"
+import image5 from "/src/assets/img/showcase/5 uk.png"
+import image6 from "/src/assets/img/showcase/6 guinness.png"
+import image7 from "/src/assets/img/showcase/7 asm.png"
+import image8 from "/src/assets/img/showcase/8 sketch.png"
+import image9 from "/src/assets/img/showcase/9 wolfdredd.png"
+import image10 from "/src/assets/img/showcase/10 warriors mag.png"
+import image11 from "/src/assets/img/showcase/10 warriors.png"
+import image12 from "/src/assets/img/showcase/11 multi.png"
+import image13 from "/src/assets/img/showcase/12 misc.png"
+import image14 from "/src/assets/img/showcase/13 misc alien.png"
+import image15 from "/src/assets/img/showcase/14 misc scary.png"
+import image16 from "/src/assets/img/showcase/15 panel.png"
+import image17 from "/src/assets/img/showcase/16 luton front.png"
+import image18 from "/src/assets/img/showcase/17 luton.png"
+import image19 from "/src/assets/img/showcase/18 luton nick.png"
+import image20 from "/src/assets/img/showcase/19 luton cave.png"
+import image21 from "/src/assets/img/showcase/20 luton crowd.png"
+import image22 from "/src/assets/img/showcase/21 www.png"
+import image23 from "/src/assets/img/showcase/22 www stacked.png"
+import image24 from "/src/assets/img/showcase/23 www row.png"
+import image25 from "/src/assets/img/showcase/24 c'art.png"
+import image26 from "/src/assets/img/showcase/25 cart cove.png"
+import image27 from "/src/assets/img/showcase/26 cart slant.png"
+import image28 from "/src/assets/img/showcase/27 lawless.png"
+import image29 from "/src/assets/img/showcase/28 NICE.png"
+import image30 from "/src/assets/img/showcase/29 Nice preacher.png"
+import image31 from "/src/assets/img/showcase/30 lucca custer.png"
+import image32 from "/src/assets/img/showcase/31 lucca castaglione.png"
+import image33 from "/src/assets/img/showcase/32 lucca multi.png"
+import image34 from "/src/assets/img/showcase/33 arseface.png"
+import image35 from "/src/assets/img/showcase/34 garth.png"
+import image36 from "/src/assets/img/showcase/35 lucca panel.png"
+
+
 
 const imageData = [
-    image1, 
-    image2, 
-    image3
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+    image11,
+    image12,
+    image13,
+    image14,
+    image15,
+    image16,
+    image17,
+    image18,
+    image19,
+    image20,
+    image21,
+    image22,
+    image23,
+    image24,
+    image25,
+    image26,
+    image27,
+    image28,
+    image29,
+    image30,
+    image31,
+    image32,
+    image33,
+    image34,
+    image35,
+    image36
 ];
 
 export default imageData;

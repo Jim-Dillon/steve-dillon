d51f3f6a1fc7a0166e1e98f0ff76defcbcf64dc5
Jim-Dillon <james.dillon94@gmail.com>
2024-04-19 14:48:25 +0100

    showcase - line up image and arrows

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,32 +1,17 @@
 import styles from '../css/showcase.module.scss';
 import { FunctionComponent } from 'react'
-import { useState } from 'react';
 import CarouselImage from './CarouselImage';
-import ArrowLeft from './ArrowLeft';
-import ArrowRight from './ArrowRight';
 
 export interface CarouselProps {
     images: string[],
+    currentImageIndex: any
 }
 
-const Carousel: FunctionComponent<CarouselProps> = ({ images }) => {
-    const [currentImageIndex, setCurrentImageIndex] = useState(0)
-
-    const goToPrevImage = () => {
-        setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1))
-    }
-
-    const goToNextImage = () => {
-        setCurrentImageIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
-      };
+const Carousel: FunctionComponent<CarouselProps> = ({ images, currentImageIndex }) => {
 
       return (
         <div className={styles.carousel}>
             <CarouselImage src={images[currentImageIndex]}/>
-            <div className={styles.carouselArrows}>
-                <ArrowLeft onClick={goToPrevImage}/>
-                <ArrowRight onClick={goToNextImage}/>
-            </div>  
         </div>
       )
 }
--- src/pages/ShowcasePage.tsx
+++ src/pages/ShowcasePage.tsx
@@ -1,15 +1,34 @@
 import styles from '/src/css/showcase.module.scss'
+import { useState } from 'react';
 import Header from "../components/Header";
 import Carousel from "../components/Carousel";
 import imageData from "../components/imageData";
+import ArrowLeft from '../components/ArrowLeft';
+import ArrowRight from '../components/ArrowRight';
 
 const ShowcasePage = () => {
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0)
+
+    const goToPrevImage = () => {
+        setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? imageData.length - 1 : prevIndex - 1))
+    }
+
+    const goToNextImage = () => {
+        setCurrentImageIndex((prevIndex) => (prevIndex === imageData.length - 1 ? 0 : prevIndex + 1));
+      };
 
   return (
     <>
       <Header />
       <div className={styles.carouselContainer}>
-        <Carousel images={imageData}/>
+        <Carousel images={imageData} currentImageIndex={currentImageIndex}/>
+      </div>
+      <div className={styles.carouselArrowsContainer}>
+        <div className={styles.carouselArrows}>
+          <ArrowLeft onClick={goToPrevImage}/>
+          <ArrowRight onClick={goToNextImage}/>
+        </div>
       </div>
     </>
   )
